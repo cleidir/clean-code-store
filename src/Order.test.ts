eb27b474d5b05d7ddf08582f56a7ed0285f546e9
Cleidir Cristiano Back <cleidir.back@gmail.com>
2021-07-28 21:43:19 -0300

    add discount coupon

--- src/Order.test.ts
+++ src/Order.test.ts
@@ -1,3 +1,4 @@
+import Coupon from "./Coupon";
 import Order from "./Order";
 
 test("Do not create a new order with invalid cpf", function () {
@@ -13,4 +14,15 @@ test("Should be create a new order using a valid cpf and 3 itens", function () {
     order.addItem("Cabo", 30, 3);
     const total = order.getTotal();
     expect(total).toBe(7090);
+});
+
+test("Should be create a new order using a discount coupon", function () {
+    const cpf = "864.464.227-84";
+    const order = new Order(cpf);
+    order.addItem("Guitarra", 1000, 2);
+    order.addItem("Amplficador", 5000, 1);
+    order.addItem("Cabo", 30, 3);
+    order.addCoupon(new Coupon("VALE20", 20));
+    const total = order.getTotal();
+    expect(total).toBe(5672);
 });
